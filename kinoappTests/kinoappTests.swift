296c20b6dcc4ff359dd88876ca16b6113b6a19a6
sabatabatadze01 <sabatabatadze15@gmail.com>
2026-01-05 23:12:13 +0400

    Initial Commit

---
+++ kinoappTests/kinoappTests.swift
@@ -0,0 +1,17 @@
+//
+//  kinoappTests.swift
+//  kinoappTests
+//
+//  Created by Saba Tabatadze on 28.12.25.
+//
+
+import Testing
+@testable import kinoapp
+
+struct kinoappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
